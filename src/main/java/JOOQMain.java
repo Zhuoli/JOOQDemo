b1b202d8c9350d957b18da7c9603c2dde213fffd
Zhuoli <zhuoliseattle@gmail.com>
2016-08-22 23:14:12 -0700

    Create table if not exist

--- src/main/java/JOOQMain.java
+++ src/main/java/JOOQMain.java
@@ -2,21 +2,30 @@
 
 // For convenience, always static import your generated tables and
 // jOOQ functions to decrease verbosity:
-        import static JooqMap.Tables.AUTHOR;
+import static JooqMap.Tables.AUTHOR;
 
-        import java.sql.*;
-        import java.util.logging.Level;
-        import java.util.logging.Logger;
+import java.sql.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-        import org.jooq.*;
-        import org.jooq.impl.*;
+import JooqMap.tables.records.AuthorRecord;
+import org.jooq.*;
+import org.jooq.impl.*;
 
-public class Main {
+public class JOOQMain {
 
     /**
      * @param args
      */
     public static void main(String[] args) {
+        new JOOQMain().Start();
+    }
+
+    public void Start()
+    {
+
         String userName = "root";
         String password = "cp8482617";
         String url = "jdbc:mysql://localhost:3306/library";
@@ -30,6 +39,11 @@ public class Main {
         // PreparedStatement and ResultSet are handled by jOOQ, internally
         try (Connection conn = DriverManager.getConnection(url, userName, password)) {
             DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+            Optional<Table<?>> table = this.GetTable(create, AUTHOR.getName());
+            if (!table.isPresent())
+            {
+                create.createTable(AUTHOR).columns(AUTHOR.fields()).execute();
+            }
             Result<Record> result = create.select().from(AUTHOR).fetch();
 
             for (Record r : result) {
@@ -45,5 +59,20 @@ public class Main {
         catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+
+    public Optional<Table<?>> GetTable(DSLContext create, String tableName)
+    {
+        return create.meta().getTables().stream().filter(p -> p.getName().equals(tableName)).findFirst();
+    }
+
+    public void AddMissingCol(DSLContext create, String tableName)
+    {
+        create.createTable(tableName).
+                column(AUTHOR.ID).
+                column(AUTHOR.FIRST_NAME).
+                column(AUTHOR.LAST_NAME).
+                execute();
     }
 }
